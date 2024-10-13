469b55de30febd452cbcdd0d610c76efe7c3650d
Roshni-seth <168473765+Roshni-seth@users.noreply.github.com>
2024-10-13 10:53:00 +0530

    Time: 5 ms (13.27%), Space: 8.4 MB (90.51%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    void generate(vector<vector<int>> &V,int l,vector<int> &v,vector<int>& nums) {
-        if(l==nums.size()) {
-            V.push_back(v);
-            return;
-        }
-        v.push_back(nums[l]);
-        generate(V,l+1,v,nums);
-        // V.push_back(v);
-        v.pop_back();
-        generate(V,l+1,v,nums);
-        // V.push_back(v);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> V;
-        vector<int> v;
-        generate(V,0,v,nums);
-        return V;
+        vector<vector<int>> ans;
+        int subsets=1<<nums.size();
+        for(int n=0;n<=subsets-1;n++) {
+            vector<int> v;
+            for(int i=0;i<nums.size();i++) {
+                if(n&(1<<i)) v.push_back(nums[i]);
+            }
+            ans.push_back(v);
+        }
+        return ans;
     }
 };
